860fb26a4e4d1e478397d994c75e0adb5133209e
athira sekharan <athirasekharan2000@gmail.com>
2024-02-14 21:05:27 +0530

    reactweb

---
+++ src/App.jsx
@@ -0,0 +1,242 @@
+import { Button, Card } from '@mui/material'
+
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+<div className=' bg-dark'>
+<div className='container'>
+  <nav class="navbar navbar-expand-lg ">
+    <div class="container-fluid">
+      <img src="./images/Ellipse 19.png" alt="" />
+      <a class="navbar-brand text-white ms-2" href="#">LIGHTS</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav  ms-auto">
+          <li class="nav-item">
+            <a class="nav-link active text-white" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#">Company</a>
+          </li>
+          <Button className='bt1 text-white' >Sign up</Button>
+  
+        </ul>
+      </div>
+    </div>
+  </nav>
+  
+  
+  
+</div>
+
+<div className='container  bg-dark'>
+
+  
+</div>
+
+
+
+  <div className='container full bg-dark'>
+    <div className='row row1 bg'>
+    <div className='col-lg-6'>
+      <h2 className='t1 text-white text-center mt-5'>Let’s Explore
+    Three-Dimensional
+    visual</h2>
+    <p className='text-white ms-5 text-center'>With virtual technology you can see the digital world feel more real and you can play the game with a new style.</p>
+    <div className='p-5 m-5 b-5'>
+        <Button className='bt1 text-white' >Get It Now</Button>
+    
+      <Button className='text-white' variant="outlined">Explore Device</Button>
+      
+    </div>
+    <img className='ms-5' src="./images/Group 1.png" alt="" /><h3 className='text-white ms-5'>400 people  online</h3>
+  
+    </div>
+    <div className='col-lg-6 gr23 mt-3'><img className='women' src="./images/Group 23.png" alt="" /></div>
+    </div>
+  
+  
+  
+  <div>
+      <div className='row row1 bg'> 
+    <div className='col-lg-6'>
+    <div className='row vow2'>
+    <div className='d1  col-lg-5 gr3'><img className='p1' src="./images/Group 3.png" alt="" /></div>
+    <div className='d2  col-lg-7 gr3'><img className='p1'  src="./images/Group 28.png" alt="" /></div>
+    </div>
+    
+    
+    </div>
+    <div className='col-lg-6 mt-5'>
+      <h2 className='t1 mt-5 text-white text-center mt-5'>New Experience In
+Playing Game</h2>
+<p className='text-white ms-5 text-center'>You can try playing the game with a
+new style and of course a more real
+feel, like you are the main character
+in your game and adventure in this
+new digital world.With virtual technology you can see the digital world feel more real and you can play the game with a new style.</p>
+<div className='container w-25 mt-5 '><Button className='bt1 text-white' >Get It Now</Button></div>
+    </div>
+      </div>
+  </div>
+    
+
+<div className='row bg-secondary'>
+<div className='col-lg-9'>
+  <h2 className='text-white t1'>Awesome experiences with
+  virtual reality world</h2>
+</div>
+<div  className='col-lg-3'><img  src="./images/Rectangle 11.png" alt="" /></div>
+</div>
+
+
+  </div>  
+
+
+<div className='container '>
+<h2 className='text-white t1 mt-5'>Mixed Reality Headsets</h2>
+</div>
+
+<div className='container'>
+  <div className='row   bg'>
+  
+  
+  <div className='col-lg-6 pic2q'><img className='pic1 container ms-5 ' src="./images/pic2.jpg" alt="" />
+  <img className='pic1 container ms-5 mt-3' src="./images/pic1.jpg" alt="" />
+     </div>
+    <div className='col-lg-6 pic2q'><img className='pic1 container ms-5' src="./images/pic3.jpg" alt="" />
+    <img className='pic1 container ms-5 mt-3' src="./images/pic4.jpg" alt="" />
+  </div>
+  </div>
+</div>
+  
+
+
+
+
+<div className='container bg mt-5'>
+  <h2 className='text-white t1 text-center'>What our clients say</h2>
+  <p className='text-white ms-5 text-center'>See what our customer say about us. It really matter for us. How good or bad
+we will make ir for evaluation to make EhyalLive better</p>
+    <div className='row'>
+  <div className='col-lg-4'><Card  className='bg-dark raww text-white p-4' variant="outlined"><h4><span className='text-warning'>StarStar</span>Star</h4>
+  <p>“I know in real-time where the money is spent,and
+  I don’t have to lend out the company’s credit card
+  anymore. What a relief!”
+  
+  </p>
+  <img src="./images/Ellipse 11.png" alt="" />Denny Hilguston</Card></div>
+  <div className='col-lg-4'>
+  
+  <Card className='bg-dark raww text-white p-4' variant="outlined"><h4><span className='text-warning'>StarStar</span>Star</h4>
+  <p>VISUALS are much better. The improvements in optics and resolution and much more than a modern console generation leap.
+  
+  </p>
+  <img src="./images/Ellipse 11.png" alt="" />Vani Pandey</Card>
+  
+  </div>
+  <div className='col-lg-4'><Card className='bg-dark raww text-white p-4'  variant="outlined"><h4><span className='text-warning'>StarStar</span>Star</h4>
+  <p>I can actually see the improvement in the graphics, not having the external sensors is a big plus not having the external sensors is a big plus.
+  .
+  
+  </p>
+  <img src="./images/Ellipse 11 (2).png" alt="" />Vani Pandey</Card></div>
+    </div>
+</div>
+
+<div className='container mt-5'>
+<div className='row rw'>
+  <div className='m2 col-lg-6'>  <h2 className='text-white t1 text-center'>Our company values
+culture   </h2>
+<p  className='text-white ms-5 text-center'>We specialize in creating visual identities for products and branda in your company.<i class="fa-solid fa-heart fa-rotate-by fa-2xl" style={{color:' #e015aa', angle:'30deg'}}></i></p>
+<i class="fa-solid fa-heart"></i>
+</div>
+  <div className='col-lg-6'>
+    <div className='row'>
+<div className='col-lg-6 text-white'><img src="./images/img1.png" alt="" style={{width:'150px'}} /><v>Be Sincere</v></div>
+<div className='col-lg-6 text-white'><img src="./images/Vector (1).png" alt="" style={{width:'150px'}} /><v>Stronger Together</v></div>
+
+<div className='col-lg-6 text-white'><img src="./images/Group 40.png" alt="" style={{width:'130px'}} /><v>Keep It Simple</v></div>
+<div className='col-lg-6 text-white'><img src="./images/Group 41.png" alt="" style={{width:'120px'}} /><v>Take Inteligent Risks</v></div>
+
+    </div>
+  </div>
+</div>
+</div>
+
+
+
+
+<div className='container'>
+<div className='row raww  mt-5'>
+<div className='col-lg-6'>
+  <h2 className='text-white text-center t1 mt-4'>Explore product in 
+new way</h2>
+<p className='text-white ms-5 text-center'>We specialize in creating visual identities for products and branda in your company.</p>
+<div className='text-center mt-5'>
+  <Button className='bt1 text-white' >START</Button>
+      
+      <Button className=' text-white' variant="outlined">YOUR EMAIL</Button>
+</div>
+</div>
+<div className='col-lg-6'><img src="./images/Group 22.png" alt="" style={{width:'100%',height:'300px'}} /></div>
+</div>
+</div>
+
+
+
+
+
+<div className='container'>
+  <div className='row'>
+<div className='col-lg-4'><h2 className='text-white text-center t1 mt-4'>lights</h2>
+<p className='text-white ms-5 text-center'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam dignissimos provident, doloribus, ea fuga reprehenderit nesciunt eius at cumque facilis delectus enim. Corrupti dolorem sequi nisi eos commodi, culpa sint.</p>
+</div>
+<div className='col-lg-2'><h4 className='text-white text-center mt-4'>QUICK LINKS</h4><p className='text-white text-center'>Robotic Automation</p>
+<p className='text-white text-center'>Education ANd Sciences</p>
+<p className='text-white text-center'>Machine Learining</p>
+<p className='text-white text-center'>prediction Analysis</p>
+<div className='text-center '>
+  <i class="fa-brands fa-whatsapp ms-3" style={{color: '#f3f4f7'}}></i>
+  <i class="fa-brands fa-twitter ms-3" style={{color: '#f3f4f7'}}></i>
+  <i class="fa-brands fa-instagram ms-3"  style={{color: '#f3f4f7'}}></i>
+  
+</div></div>
+<div className='col-lg-2'><h4 className='text-white text-center mt-4'>COMPANY</h4><p className='text-white text-center'><b>phone number</b></p>
+<div className='text-center'>
+  <i className="fa-solid fa-phone" style={{color: '#fafcff'}}></i>
+  
+</div>
+<p className='text-white text-center mt-4'>654548486</p>
+<p className='text-white text-center'><b>Email us</b></p>
+<div className='text-center' >
+  <i class="fa-solid fa-envelope" style={{color: '#f3f4f7'}}></i>
+  
+</div><p className='text-white text-center mt-4'>light@gmail.com</p>
+</div>
+
+
+<div className='col-lg-4'>
+<h4 className='text-white text-center mt-4'>NEWSLETTER</h4><p className='text-white text-center'><b>Lorem ipsum dolor sit amet consectetur adipisicing elit. A deleniti aliquid officia dolor, perferendis aut necessitatibus quia repudiandae amet unde? Nam, dolor</b></p>
+
+</div>
+
+  </div>
+</div>
+
+</div> 
+<hr />
+ </>
+  )
+}
+
+export default App
